2d7914b3086074cd9036bccfecfa6b3725131eed
super-website <kh.muhavia1@gmail.com>
2025-03-13 01:32:58 +0500

    Initial commit

--- app/(admin)/control/gallery/edit-gallery/[id]/page.tsx
+++ app/(admin)/control/gallery/edit-gallery/[id]/page.tsx
@@ -5,5 +5,9 @@ import React from 'react'
 export default async function page({ params }: { params: { id: string } }) {
   const { id } = params
   const data = await getSingleGallery(id)
+
+  if (!data) {
+    return <p>Gallery not found.</p>
+  }
   return <EditGalleryForm data={data} />
 }

53c389890232b0f4686661d7ea4a90dede6bb43d
super-website <kh.muhavia1@gmail.com>
2025-03-16 23:43:55 +0500

    update metadata

--- components/AddNewTopic.tsx
+++ components/AddNewTopic.tsx
@@ -1,77 +1,102 @@
-"use client";
-import React from "react";
-import { useSearchParams } from "next/navigation";
-import { createTopic } from "@/utils/actions";
-
+'use client'
+import React, { useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { createTopic } from '@/utils/actions'
+import ReactSimpleMDEEditor from 'react-simplemde-editor'
+import 'simplemde/dist/simplemde.min.css'
 interface Subject {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
 export default function AddNewTopic({ subjects }: { subjects: Subject[] }) {
-  const searchParams = useSearchParams();
-  const success = searchParams.get("success");
+  const searchParams = useSearchParams()
+  const success = searchParams.get('success')
+
+  const [longDesc, setLongDesc] = useState('')
+
+  const handleEditorChange = (value: string) => {
+    setLongDesc(value)
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    formData.append('long_desc', longDesc)
+
+    try {
+      await createTopic(formData)
+      console.log('Topic created successfully!')
+    } catch (error) {
+      console.error('Error creating topic:', error)
+    }
+  }
+
   return (
-    <div className="p-6 max-w-6xl mx-auto bg-white rounded-lg shadow-sm">
-      <h1 className="text-3xl font-semibold mb-4">Add Topic</h1>
+    <div className='p-6 max-w-6xl mx-auto bg-white rounded-lg shadow-sm'>
+      <h1 className='text-3xl font-semibold mb-4'>Add Topic</h1>
 
       {success && (
-        <div className="alert alert-success mb-4">
+        <div className='alert alert-success mb-4'>
           Topic added successfully!
         </div>
       )}
 
-      <form action={createTopic} method="POST" className="space-y-4">
-        <div className="form-control">
-          <label htmlFor="title" className="block text-sm font-medium">
+      <form onSubmit={handleSubmit} method='POST' className='space-y-4'>
+        <div className='form-control'>
+          <label htmlFor='title' className='block text-sm font-medium'>
             Title
           </label>
           <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Enter Your Title"
-            className="input input-bordered w-full mt-1"
+            type='text'
+            id='title'
+            name='title'
+            placeholder='Enter Your Title'
+            className='input input-bordered w-full mt-1'
             required
           />
         </div>
 
-        <div className="form-control">
-          <label htmlFor="short_desc" className="block text-sm font-medium">
+        <div className='form-control'>
+          <label htmlFor='short_desc' className='block text-sm font-medium'>
             Short Description
           </label>
           <input
-            type="text"
-            id="short_desc"
-            name="short_desc"
-            placeholder="Enter Your Short Description"
-            className="input input-bordered w-full mt-1"
+            type='text'
+            id='short_desc'
+            name='short_desc'
+            placeholder='Enter Your Short Description'
+            className='input input-bordered w-full mt-1'
             required
           />
         </div>
 
-        <div className="form-control">
-          <label htmlFor="long_desc" className="block text-sm font-medium">
+        <div className='form-control'>
+          <label htmlFor='long_desc' className='block text-sm font-medium'>
             Long Description
           </label>
-          <textarea
-            id="long_desc"
-            name="long_desc"
-            placeholder="Enter Your Long Description"
-            className="textarea textarea-bordered w-full mt-1"
-            required
-          ></textarea>
+
+          <ReactSimpleMDEEditor
+            value={longDesc}
+            onChange={handleEditorChange}
+            options={{
+              spellChecker: false,
+              maxHeight: '400px',
+            }}
+          />
         </div>
 
-        <div className="form-control">
-          <label className="block text-sm font-medium">Subject</label>
+        <div className='form-control'>
+          <label className='block text-sm font-medium'>Subject</label>
           <select
-            name="subjectId"
-            id="subjectId"
-            className="select select-bordered w-full mt-1"
+            name='subjectId'
+            id='subjectId'
+            className='select select-bordered w-full mt-1'
             required
           >
-            <option value="" disabled selected>
+            <option value='' disabled selected>
               Select a Subject
             </option>
             {subjects.map((subject) => (
@@ -82,10 +107,10 @@ export default function AddNewTopic({ subjects }: { subjects: Subject[] }) {
           </select>
         </div>
 
-        <button type="submit" className="btn btn-primary w-full">
+        <button type='submit' className='btn btn-primary w-full'>
           Submit
         </button>
       </form>
     </div>
-  );
+  )
 }

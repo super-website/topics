f6330e1b687d36a31d240b0e65ecfc41fe23afd9
super-website <kh.muhavia1@gmail.com>
2025-03-17 16:57:51 +0500

    update metadata

--- utils/actions.ts
+++ utils/actions.ts
@@ -312,7 +312,7 @@ export const logout = async () => {
   cookies().delete('role')
   redirect('/control/login')
 }
-export const createGallery = async (formData: FormData) => {
+export const createGallery = async (formData: FormData): Promise<void> => {
   const files = formData.getAll('files') as File[]
   const title = formData.get('title') as string
 
@@ -350,11 +350,16 @@ export const createGallery = async (formData: FormData) => {
         images: uploadedImages,
       },
     })
+
+    redirect('/control/gallery/add-gallery?success=true')
+    revalidatePath('/control/gallery')
   } catch (error) {
-    console.error('Upload failed:', error)
+    console.error(
+      'Upload failed:',
+      error instanceof Error ? error.message : error
+    )
     throw new Error('Gallery creation failed')
   }
-  redirect('/control/gallery/add-gallery?success=true')
 }
 
 export const getAllGallery = async () => {

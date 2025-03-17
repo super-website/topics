ddaf689952374f5894748d1cdd47882c38db8e9f
super-website <kh.muhavia1@gmail.com>
2025-03-17 21:24:33 +0500

    update metadata

--- app/(frontend)/subjects/page.tsx
+++ app/(frontend)/subjects/page.tsx
@@ -1,39 +1,25 @@
-'use server'
 import { getAllSubject } from '@/utils/actions'
 import Link from 'next/link'
 
-export const metadata = {
-  title: 'Subjects - Education With US',
-  description:
-    'Explore various MBA subjects with detailed topics to enhance your learning experience.',
-  keywords:
-    'MBA, MBA subjects, online education, learning, study resources, business management',
-  author: 'Ameer Muhavia',
-}
-
-export default async function Page() {
+export default async function Subjects() {
   const subjects = await getAllSubject()
 
   return (
-    <div className='max-w-2xl mx-auto px-4 py-6'>
-      <h1 className='text-2xl font-semibold mb-4'>Subjects</h1>
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-        {subjects.map((subject) => (
-          <div
-            key={subject.id}
-            className='card border rounded-none p-4 bg-base-100'
-          >
-            <Link
-              href={`/subject/${subject.id}`}
-              className='text-blue-600 font-medium text-lg hover:underline cursor-pointer'
-            >
-              {subject.short_name}
-            </Link>
-            <p className='text-gray-500 text-sm'>
-              Contains ({subject.topics.length}) topics.
-            </p>
-          </div>
-        ))}
+    <div className='drawer hidden md:flex'>
+      <div className='drawer-content'>
+        <h2 className='font-bold text-xl'>Subjects</h2>
+        <ul className='menu text-base-content min-h-full w-64 p-4'>
+          {subjects.map((subject) => (
+            <li key={subject.id}>
+              <Link
+                href={`/subject/${subject.id}`}
+                className='border-b border-gray-300'
+              >
+                {subject.short_name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )

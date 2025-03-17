45ca0fdfadf4bc445ecefa36c7ba1fa1406492e0
super-website <kh.muhavia1@gmail.com>
2025-03-17 21:20:23 +0500

    update metadata

--- components/HomerNavbar.tsx
+++ components/HomerNavbar.tsx
@@ -46,7 +46,7 @@ export default function HomeNavbar() {
           </Link>
         </div>
 
-        <div className='navbar-center invisible md:visible'>
+        <div className='navbar-center hidden md:inline-block'>
           <input
             type='text'
             value={searchTerm}

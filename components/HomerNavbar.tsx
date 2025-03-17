17260040dafa172871e97ac6edde7f95b1b85c99
super-website <kh.muhavia1@gmail.com>
2025-03-17 22:37:33 +0500

    update metadata

--- components/HomerNavbar.tsx
+++ components/HomerNavbar.tsx
@@ -57,7 +57,7 @@ export default function HomeNavbar() {
         </div>
 
         <div className='navbar-end visible md:invisible'>
-          <div className='dropdown dropdown-end'>
+          <div className='dropdown dropdown-left'>
             <div
               tabIndex={0}
               role='button'

989470fd78ce6b65d5b33f2efe77ba2f376329b8
super-website <kh.muhavia1@gmail.com>
2025-03-18 16:23:20 +0500

    update metadata

--- components/HomerNavbar.tsx
+++ components/HomerNavbar.tsx
@@ -56,12 +56,12 @@ export default function HomeNavbar() {
           />
         </div>
 
-        <div className='navbar-end visible md:invisible pr-4 md:pr-0'>
+        <div className='navbar-end visible md:invisible '>
           <div className='dropdown dropdown-left'>
             <div
               tabIndex={0}
               role='button'
-              className='btn btn-ghost btn-circle'
+              className='btn btn-ghost btn-circle pr-4 md:pr-0'
             >
               <svg
                 xmlns='http://www.w3.org/2000/svg'

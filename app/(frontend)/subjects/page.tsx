bf680253995abc1936071367c0fea5ef5e3ac19e
super-website <kh.muhavia1@gmail.com>
2025-03-18 19:53:00 +0500

    update metadata

--- app/(frontend)/subjects/page.tsx
+++ app/(frontend)/subjects/page.tsx
@@ -1,3 +1,4 @@
+import HomeSubject from '@/components/HomeSubject'
 import { getAllSubject } from '@/utils/actions'
 import Link from 'next/link'
 
@@ -10,30 +11,35 @@ export const metadata = {
   author: 'Ameer Muhavia',
 }
 
-export default async function Page() {
-  const subjects = await getAllSubject()
+interface Topic {
+  id: string
+  short_desc: string
+  title: string
+  long_desc: string
+  createdAt: Date
+  subjectId: string | null
+}
 
-  return (
-    <div className='max-w-2xl mx-auto px-4 py-6'>
-      <h1 className='text-2xl font-semibold mb-4'>Subjects</h1>
-      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-        {subjects.map((subject) => (
-          <div
-            key={subject.id}
-            className='card border rounded-none p-4 bg-base-100'
-          >
-            <Link
-              href={`/subject/${subject.id}`}
-              className='text-blue-600 font-medium text-lg hover:underline cursor-pointer'
-            >
-              {subject.short_name}
-            </Link>
-            <p className='text-gray-500 text-sm'>
-              Contains ({subject.topics.length}) topics.
-            </p>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+interface Subject {
+  id: string
+  short_name: string
+  name: string
+  short_desc: string
+  topics: Topic[]
+}
+
+export default async function Page() {
+  let subjects: Subject[] = []
+
+  try {
+    subjects = await getAllSubject()
+  } catch (error) {
+    console.error('Error fetching subjects:', error)
+  }
+
+  if (subjects.length === 0) {
+    return <p>No subjects found. Please try again later.</p>
+  }
+
+  return <HomeSubject subjects={subjects} />
 }

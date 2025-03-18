c487ff9199e12846604d6ece1d6fe2bf6a0b3f4d
super-website <kh.muhavia1@gmail.com>
2025-03-18 19:56:37 +0500

    update metadata

--- app/(frontend)/subjects/page.tsx
+++ app/(frontend)/subjects/page.tsx
@@ -1,45 +1,39 @@
-import HomeSubject from '@/components/HomeSubject'
 import { getAllSubject } from '@/utils/actions'
 import Link from 'next/link'
 
-export const metadata = {
-  title: 'Subjects - Education With US',
-  description:
-    'Explore various MBA subjects with detailed topics to enhance your learning experience.',
-  keywords:
-    'MBA, MBA subjects, online education, learning, study resources, business management',
-  author: 'Ameer Muhavia',
-}
-
-interface Topic {
-  id: string
-  short_desc: string
-  title: string
-  long_desc: string
-  createdAt: Date
-  subjectId: string | null
-}
-
-interface Subject {
-  id: string
-  short_name: string
-  name: string
-  short_desc: string
-  topics: Topic[]
-}
+// export const metadata = {
+//   title: 'Subjects - Education With US',
+//   description:
+//     'Explore various MBA subjects with detailed topics to enhance your learning experience.',
+//   keywords:
+//     'MBA, MBA subjects, online education, learning, study resources, business management',
+//   author: 'Ameer Muhavia',
+// }
 
 export default async function Page() {
-  let subjects: Subject[] = []
+  const subjects = await getAllSubject()
 
-  try {
-    subjects = await getAllSubject()
-  } catch (error) {
-    console.error('Error fetching subjects:', error)
-  }
-
-  if (subjects.length === 0) {
-    return <p>No subjects found. Please try again later.</p>
-  }
-
-  return <HomeSubject subjects={subjects} />
+  return (
+    <div className='max-w-2xl mx-auto px-4 py-6'>
+      <h1 className='text-2xl font-semibold mb-4'>Subjects</h1>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+        {subjects.map((subject) => (
+          <div
+            key={subject.id}
+            className='card border rounded-none p-4 bg-base-100'
+          >
+            <Link
+              href={`/subject/${subject.id}`}
+              className='text-blue-600 font-medium text-lg hover:underline cursor-pointer'
+            >
+              {subject.short_name}
+            </Link>
+            <p className='text-gray-500 text-sm'>
+              Contains ({subject.topics.length}) topics.
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }

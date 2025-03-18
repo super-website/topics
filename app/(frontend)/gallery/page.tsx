cf004aad0bd2165f0cff5398dd99968d3caa6153
super-website <kh.muhavia1@gmail.com>
2025-03-18 22:37:27 +0500

    update metadata

--- app/(frontend)/gallery/page.tsx
+++ app/(frontend)/gallery/page.tsx
@@ -1,6 +1,8 @@
 import { getAllGallery } from '@/utils/actions'
 import Image from 'next/image'
 
+export const revalidate = 0
+
 export default async function Page() {
   const data = await getAllGallery()
   return (

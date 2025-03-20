e09b18dc63b0a98a4f264fd5c7fad001de66f73f
super-website <kh.muhavia1@gmail.com>
2025-03-21 00:47:11 +0500

    update metadata

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,29 @@
 import { getAllSubject, getAllTopics } from '@/utils/actions'
 import { MetadataRoute } from 'next'
 
+interface Subject {
+  id: string
+  name: string
+  short_name: string
+  short_desc: string
+  tags: string[]
+}
+
+interface Topic {
+  id: string
+  title: string
+  short_desc: string
+  createdAt: Date
+  subject?: Subject | null
+  tags: string[]
+}
+
 export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
-  const subjects = await getAllSubject()
+  const subjects: Subject[] = await getAllSubject()
   const topics = await getAllTopics('')
+  const topicsInside: Topic[] = Array.isArray(topics)
+    ? topics
+    : topics.topics || []
 
   const urls = subjects.map((subject) => ({
     url: `${process.env.BASE_URL}/subject/${subject.id}`,
@@ -11,7 +31,7 @@ export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
     priority: 0.8,
   }))
 
-  const topicUrls = topics.map((topic) => ({
+  const topicUrls = topicsInside.map((topic) => ({
     url: `${process.env.BASE_URL}/topic/${topic.id}`,
     changeFrequency: 'daily' as const,
     priority: 0.7,

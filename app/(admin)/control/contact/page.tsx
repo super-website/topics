50fe2372c8e5f470e8f1b381ff001691daa20913
super-website <kh.muhavia1@gmail.com>
2025-03-21 21:14:37 +0500

    update metadata

--- app/(admin)/control/contact/page.tsx
+++ app/(admin)/control/contact/page.tsx
@@ -22,7 +22,7 @@ export default async function Page() {
             No comments available.
           </p>
         ) : (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          <div className='table w-full table-zebra'>
             {data.map((comment) => (
               <div
                 key={comment.id}

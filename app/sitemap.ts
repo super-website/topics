c0824814292798ba9066a930569ad9bc6504f81d
super-website <kh.muhavia1@gmail.com>
2025-03-21 15:00:41 +0500

    update metadata

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -26,20 +26,20 @@ export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
     : topics.topics || []
 
   const urls = subjects.map((subject) => ({
-    url: `${process.env.BASE_URL}/subject/${subject.id}`,
+    url: `https://educationwithhamza.vercel.app/subject/${subject.id}`,
     changeFrequency: 'daily' as const,
     priority: 0.8,
   }))
 
   const topicUrls = topicsInside.map((topic) => ({
-    url: `${process.env.BASE_URL}/topic/${topic.id}`,
+    url: `https://educationwithhamza.vercel.app/topic/${topic.id}`,
     changeFrequency: 'daily' as const,
     priority: 0.7,
   }))
 
   return [
     {
-      url: `${process.env.BASE_URL}/`,
+      url: `https://educationwithhamza.vercel.app`,
       lastModified: new Date().toISOString(),
       changeFrequency: 'daily',
       priority: 1,

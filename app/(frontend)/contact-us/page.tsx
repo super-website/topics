c09571c8ba30193e93be040b4135caaf605be54f
super-website <kh.muhavia1@gmail.com>
2025-03-21 20:37:56 +0500

    update metadata

--- app/(frontend)/contact-us/page.tsx
+++ app/(frontend)/contact-us/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div className='container mx-auto p-5'>
+    <div className='max-w-4xl mx-auto p-5 '>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
         <div className='space-y-5'>
           <h2 className='text-2xl font-bold'>Leave a Message</h2>

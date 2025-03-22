7d6160a57576044e9f63cc5b6ba73a47f300ce4a
super-website <kh.muhavia1@gmail.com>
2025-03-22 22:25:42 +0500

    update metadata

--- app/(admin)/control/contact/page.tsx
+++ app/(admin)/control/contact/page.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
       </div>
 
       <div
-        className='grid grid-cols-1 gap-6'
+        className='grid grid-cols-1 md:grid-cols-3 gap-6'
         style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))' }}
       >
         {data.length === 0 ? (

a7c1a8bfe8ce42a29ee08a8f0a3eab8c8464277d
super-website <kh.muhavia1@gmail.com>
2025-03-22 21:35:04 +0500

    update metadata

--- utils/actions.ts
+++ utils/actions.ts
@@ -521,6 +521,8 @@ export const createPdf = async (formData: FormData) => {
         url: result.secure_url,
       },
     })
+
+    redirect('/control/pdf')
   } catch (error: any) {
     console.error('Error creating PDF:', error)
 

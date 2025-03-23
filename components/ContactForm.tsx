d4c02992ddb5fb2542f0ead56f259ebdb5aea01d
super-website <kh.muhavia1@gmail.com>
2025-03-23 20:19:00 +0500

    update metadata

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { createComment } from '@/utils/actions'
+import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 import React, { use } from 'react'
 
@@ -51,39 +52,57 @@ export default function ContactForm() {
           <h2 className='text-2xl font-bold'>Reach Us</h2>
           <p className='text-gray-700'>
             Email:{' '}
-            <a href='mailto:kamoffical@notes.com' className='text-blue-600'>
-              kamoffical@notes.com
+            <a
+              href='mailto:khanrajahamza52@gmail.com'
+              className='text-blue-600'
+            >
+              khanrajahamza52@gmail.com
             </a>
           </p>
           <p className='text-gray-700'>
             Phone:{' '}
             <a href='tel:+000000000' className='text-blue-600'>
-              +00 000000000
+              +92 341 0188551
             </a>
           </p>
           <p className='text-gray-700'>
             Address:{' '}
             <span className='text-blue-600'>
-              1234, Main Street, City, Country
+              1234, Main Street, Muzaffrabad, AJK
             </span>
           </p>
 
           <h3>Social Media</h3>
 
           <div className='space-x-3'>
-            <a
-              href='https://facebook.com'
-              className='text-blue-600 hover:underline'
+            <Link
+              href='https://www.youtube.com/@EducationWithHamza-g8v'
+              target='_blank'
             >
-              Facebook
-            </a>
-
-            <a
-              href='https://twitter.com'
-              className='text-blue-600 hover:underline'
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                className='fill-current'
+              >
+                <path d='M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z'></path>
+              </svg>
+            </Link>
+            <Link
+              href='https://www.facebook.com/profile.php?id=61566559002640'
+              target='_blank'
             >
-              Twitter
-            </a>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                className='fill-current'
+              >
+                <path d='M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z'></path>
+              </svg>
+            </Link>
           </div>
         </div>
       </div>

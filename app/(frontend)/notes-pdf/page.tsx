b71c98c8e6bacdf904ccab1a3f02dcba2c87f5f4
super-website <kh.muhavia1@gmail.com>
2025-03-24 22:42:39 +0500

    update metadata

--- app/(frontend)/notes-pdf/page.tsx
+++ app/(frontend)/notes-pdf/page.tsx
@@ -3,6 +3,15 @@ import React from 'react'
 import { getAllPdf } from '@/utils/actions'
 import PDFCard from '@/components/PDFCard'
 
+export const metadata = {
+  title: 'Educational Resources',
+  description:
+    'Explore a comprehensive gallery of student notes, study materials, and educational resources in PDF format, shared by students and educators.',
+  keywords:
+    'student notes, study resources, educational PDFs, online learning materials, classroom resources, notes gallery, teacher and student notes, study guides',
+  author: 'Ameer Muhavia',
+}
+
 interface Pdf {
   id: string
   title: string

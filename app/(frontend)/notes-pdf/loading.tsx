5a63e21107e87f25a8f79286b2045c5d65395c14
super-website <kh.muhavia1@gmail.com>
2025-03-25 23:12:40 +0500

    update metadata

--- app/(frontend)/notes-pdf/loading.tsx
+++ app/(frontend)/notes-pdf/loading.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export default function loading() {
-  return <div>Loading....</div>
+  return <div className="mx-10 md:mx-0">
+    <span className=""></span>
+  </div>
 }

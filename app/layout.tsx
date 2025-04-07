f25585949170b66ac6f6de6584a954695eeb8e5b
super-website <kh.muhavia1@gmail.com>
2025-04-07 22:12:27 +0500

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={hind.className}>
+      <head>
+        {process.env.NODE_ENV === "production" && (
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7339717436236652"
+            crossOrigin="anonymous"
+          ></script>
+        )}
+      </head>
       <body className="bg-gray-200">
         {children}
 
---
+++ components/Adsense.tsx
@@ -0,0 +1,11 @@
+import Script from "next/script";
+import React from "react";
+
+export default function Adsense() {
+  return (
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7339717436236652"
+    />
+  );
+}

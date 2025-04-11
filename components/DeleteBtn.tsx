d1e785d5f9e8c75ced5459d50842fa9f96c8e567
super-website <kh.muhavia1@gmail.com>
2025-04-11 18:55:30 +0500

    update metadata

--- components/DeleteBtn.tsx
+++ components/DeleteBtn.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useFormStatus } from "react-dom";
 
 export default function DeleteBtn({
   id,
   deleteAction,
 }: {
-  id: string
-  deleteAction: (formData: FormData) => Promise<void>
+  id: string;
+  deleteAction: (formData: FormData) => Promise<void>;
 }) {
+  const { pending } = useFormStatus();
   return (
     <form action={deleteAction}>
-      <input type='hidden' name='id' value={id} />
-      <button type='submit' className='btn btn-error'>
-        Delete
+      <input type="hidden" name="id" value={id} />
+      <button type="submit" className="btn btn-error" disabled={pending}>
+        {pending ? <span className="loading loading-spinner"></span> : "Delete"}
       </button>
     </form>
-  )
+  );
 }

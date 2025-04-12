02f2d2936eef1c531259a5b9093ebaad5151144f
super-website <kh.muhavia1@gmail.com>
2025-04-12 23:54:37 +0500

    update metadata

--- app/(admin)/control/gallery/page.tsx
+++ app/(admin)/control/gallery/page.tsx
@@ -1,49 +1,50 @@
-import { getAllGallery, deleteGallery } from '@/utils/actions'
-import Image from 'next/image'
-import Link from 'next/link'
+import DeleteBtn from "@/components/DeleteBtn";
+import { getAllGallery, deleteGallery } from "@/utils/actions";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Page() {
-  const galleries = await getAllGallery()
+  const galleries = await getAllGallery();
 
   return (
-    <div className='max-w-5xl mx-auto p-6'>
-      <div className='flex justify-between items-center flex-wrap'>
-        <h1 className='text-sm uppercase font-semibold'>Gallery</h1>
-        <Link href='/control/gallery/add-gallery' className='btn btn-primary'>
+    <div className="max-w-5xl mx-auto p-6">
+      <div className="flex justify-between items-center flex-wrap">
+        <h1 className="text-sm uppercase font-semibold">Gallery</h1>
+        <Link href="/control/gallery/add-gallery" className="btn btn-primary">
           Add New
         </Link>
       </div>
 
       {galleries.length === 0 ? (
-        <p className='text-gray-500 mt-4'>No galleries available.</p>
+        <p className="text-gray-500 mt-4">No galleries available.</p>
       ) : (
-        <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6'>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
           {galleries.map((gallery) => {
-            let images: { public_id: string; secure_url: string }[] = []
+            let images: { public_id: string; secure_url: string }[] = [];
 
             try {
-              if (typeof gallery.images === 'string') {
-                images = JSON.parse(gallery.images)
+              if (typeof gallery.images === "string") {
+                images = JSON.parse(gallery.images);
               } else if (Array.isArray(gallery.images)) {
                 images = gallery.images as {
-                  public_id: string
-                  secure_url: string
-                }[]
+                  public_id: string;
+                  secure_url: string;
+                }[];
               }
             } catch (error) {
-              console.error('Error parsing images:', error)
+              console.error("Error parsing images:", error);
             }
 
             return (
               <li
                 key={gallery.id}
-                className='bg-white p-4 rounded-lg shadow-md relative'
+                className="bg-white p-4 rounded-lg shadow-md relative"
               >
-                <h2 className='text-lg font-semibold text-center mb-2'>
+                <h2 className="text-lg font-semibold text-center mb-2">
                   {gallery.title}
                 </h2>
 
-                <div className='flex flex-wrap gap-2'>
+                <div className="flex flex-wrap gap-2">
                   {images.length > 0 ? (
                     images.map((image) => (
                       <>
@@ -53,33 +54,25 @@ export default async function Page() {
                           alt={gallery.title}
                           width={200}
                           height={100}
-                          className='rounded-md object-cover'
+                          className="rounded-md object-cover"
                         />
                       </>
                     ))
                   ) : (
-                    <p className='text-gray-500 text-sm'>
+                    <p className="text-gray-500 text-sm">
                       No images available.
                     </p>
                   )}
                 </div>
 
-                <div className='flex justify-between mt-4'>
-                  <form action={deleteGallery} method='POST'>
-                    <input type='hidden' name='galleryId' value={gallery?.id} />
-                    <button
-                      type='submit'
-                      className='px-3 py-2 bg-red-500 text-white rounded-md text-sm hover:bg-red-600'
-                    >
-                      Delete
-                    </button>
-                  </form>
+                <div className="flex justify-between mt-4">
+                  <DeleteBtn deleteAction={deleteGallery} id={gallery.id} />
                 </div>
               </li>
-            )
+            );
           })}
         </ul>
       )}
     </div>
-  )
+  );
 }

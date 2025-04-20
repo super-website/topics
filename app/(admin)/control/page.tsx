4c5ac52a9cd28bdee009b75b00381123cca498c3
super-website <kh.muhavia1@gmail.com>
2025-04-20 22:51:42 +0500

    update metadata

--- app/(admin)/control/page.tsx
+++ app/(admin)/control/page.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { getAllPdf, getAllSubject } from "@/utils/actions";
+import { getAllPdf, getAllSubject, getAllTopics } from "@/utils/actions";
 
 export const revalidate = 60;
 
 export default async function page() {
-  const data = await getAllSubject();
+  const topics = await getAllTopics("");
   const pdfs = await getAllPdf("");
+  const subjects = await getAllSubject();
   return (
     <div>
       <h1 className="m-10 ml-0 text-3xl">Dashboard</h1>
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-10">
         <div className="stats stats-vertical p-8 ">
           <div className="stat-title">Total Subjects</div>
-          <div className="stat-value p-4 text-center">{data.length}</div>
+          <div className="stat-value p-4 text-center">{topics.length}</div>
           <div className="stat-actions">
             <span className="stat-desc">Published</span>
           </div>
         </div>
         <div className="stats stats-vertical p-8 ">
           <div className="stat-title">Total Subjects</div>
-          <div className="stat-value p-4 text-center">{data.length}</div>
+          <div className="stat-value p-4 text-center">{subjects.length}</div>
           <div className="stat-actions">
             <span className="stat-desc">Published</span>
           </div>

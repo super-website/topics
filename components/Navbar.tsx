c2821a41f84a02c26f3c4141e6ff42202f08fea6
super-website <kh.muhavia1@gmail.com>
2025-04-20 23:39:48 +0500

    update metadata

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,44 @@
-'use client'
-import { logout } from '@/utils/actions'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { logout } from "@/utils/actions";
+import Link from "next/link";
+import React from "react";
 
 const lists = [
-  { id: 1, name: 'topics', url: '/control/topics' },
-  { id: 2, name: 'subjects', url: '/control/subjects' },
-  { id: 3, name: 'gallery', url: '/control/gallery' },
-  { id: 4, name: 'pdf', url: '/control/pdf' },
-  { id: 5, name: 'comments', url: '/control/contact' },
-]
+  { id: 1, name: "topics", url: "/control/topics" },
+  { id: 2, name: "subjects", url: "/control/subjects" },
+  { id: 3, name: "gallery", url: "/control/gallery" },
+  { id: 4, name: "pdf", url: "/control/pdf" },
+  { id: 5, name: "comments", url: "/control/contact" },
+];
 
 export default function Navbar() {
   return (
-    <nav className='bg-base-200  shadow-md'>
-      <div className='max-w-7xl mx-auto navbar'>
-        <div className='navbar-start'>
-          <Link href='/control' className='text-2xl font-bold'>
-            C<span className='text-primary'>M</span>S
+    <nav className="bg-base-200 fixed top-0 shadow-md">
+      <div className="max-w-7xl mx-auto navbar">
+        <div className="navbar-start">
+          <Link href="/control" className="text-2xl font-bold">
+            C<span className="text-primary">M</span>S
           </Link>
         </div>
-        <div className='navbar-center'>
-          <ul className='flex space-x-4'>
+        <div className="navbar-center">
+          <ul className="flex space-x-4">
             {lists.map((item) => {
               return (
                 <li key={item.id}>
                   <Link
                     href={item.url}
-                    className='hover:text-primary capitalize'
+                    className="hover:text-primary capitalize"
                   >
                     {item.name}
                   </Link>
                 </li>
-              )
+              );
             })}
           </ul>
         </div>
-        <div className='navbar-end'>
+        <div className="navbar-end">
           <button
-            className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600'
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
             onClick={async () => await logout()}
           >
             Logout
@@ -46,5 +46,5 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }

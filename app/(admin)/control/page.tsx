da2d592c505223e27774868d9c4552a68fbfda78
super-website <kh.muhavia1@gmail.com>
2025-04-20 22:44:25 +0500

    update metadata

--- app/(admin)/control/page.tsx
+++ app/(admin)/control/page.tsx
@@ -1,36 +1,37 @@
-import { getAllSubject } from '@/utils/actions'
-import React from 'react'
+import React from "react";
+import { getAllPdf, getAllSubject } from "@/utils/actions";
 
-export const revalidate = 0
+export const revalidate = 60;
 
 export default async function page() {
-  const data = await getAllSubject()
+  const data = await getAllSubject();
+  const pdfs = await getAllPdf("");
   return (
     <div>
-      <h1 className='m-10 ml-0 text-3xl'>Dashboard</h1>
-      <div className='grid lg:grid-cols-3 grid-cols-1 gap-10'>
-        <div className='stats stats-vertical p-8 '>
-          <div className='stat-title'>Total Subjects</div>
-          <div className='stat-value p-4 text-center'>{data.length}</div>
-          <div className='stat-actions'>
-            <span className='stat-desc'>Published</span>
+      <h1 className="m-10 ml-0 text-3xl">Dashboard</h1>
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-10">
+        <div className="stats stats-vertical p-8 ">
+          <div className="stat-title">Total Subjects</div>
+          <div className="stat-value p-4 text-center">{data.length}</div>
+          <div className="stat-actions">
+            <span className="stat-desc">Published</span>
           </div>
         </div>
-        <div className='stats stats-vertical p-8 '>
-          <div className='stat-title'>Total Subjects</div>
-          <div className='stat-value p-4 text-center'>{data.length}</div>
-          <div className='stat-actions'>
-            <span className='stat-desc'>Published</span>
+        <div className="stats stats-vertical p-8 ">
+          <div className="stat-title">Total Subjects</div>
+          <div className="stat-value p-4 text-center">{data.length}</div>
+          <div className="stat-actions">
+            <span className="stat-desc">Published</span>
           </div>
         </div>
-        <div className='stats stats-vertical p-8 '>
-          <div className='stat-title'>Total Subjects</div>
-          <div className='stat-value p-4 text-center'>{data.length}</div>
-          <div className='stat-actions'>
-            <span className='stat-desc'>Published</span>
+        <div className="stats stats-vertical p-8 ">
+          <div className="stat-title">Total Pdfs</div>
+          <div className="stat-value p-4 text-center">{data.length}</div>
+          <div className="stat-actions">
+            <span className="stat-desc">{pdfs.length}</span>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }

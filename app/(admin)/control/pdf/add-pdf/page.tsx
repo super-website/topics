34ec8c68b780ed181d5d5dd9d9a0eed93c2c08f6
super-website <kh.muhavia1@gmail.com>
2025-04-22 16:16:53 +0500

    update metadata

--- app/(admin)/control/pdf/add-pdf/page.tsx
+++ app/(admin)/control/pdf/add-pdf/page.tsx
@@ -1,39 +1,35 @@
-import { createPdf } from '@/utils/actions'
-import React from 'react'
+import SubmitBtn from "@/components/SubmitBtn";
+import { createPdf } from "@/utils/actions";
+import React from "react";
 
 export default function page() {
   return (
     <div>
-      <form action={createPdf} method='post'>
-        <h1 className='text-2xl font-semibold'>Add Pdf</h1>
+      <form action={createPdf} method="post">
+        <h1 className="text-2xl font-semibold">Add Pdf</h1>
 
-        <label className='block'>
-          <span className='font-medium text-gray-700'>Title</span>
+        <label className="block my-5">
+          <span className="font-medium text-gray-700">Title</span>
           <input
-            name='title'
+            name="title"
             required
-            className='w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-            placeholder='Enter pdf title'
+            className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter pdf title"
           />
         </label>
 
-        <label className='block'>
-          <span className='font-medium text-gray-700'>Pdf</span>
-          <input
-            name='pdf'
-            type='file'
-            required
-            className='w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-          />
-        </label>
+        <div className="mb-4">
+          <label
+            htmlFor="file-upload"
+            className="border-2 border-dashed border-blue-400 h-48 w-full flex items-center justify-center text-center text-lg text-gray-700 cursor-pointer"
+          >
+            Drop Pdf here to upload
+            <input id="file-upload" name="pdf" type="file" className="hidden" />
+          </label>
+        </div>
 
-        <button
-          type='submit'
-          className='w-full px-3 py-2 mt-1 text-white bg-blue-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-        >
-          Add Pdf
-        </button>
+        <SubmitBtn />
       </form>
     </div>
-  )
+  );
 }

a7fad6bcb7c5c7d4c917997f493c3a7e386a8716
super-website <kh.muhavia1@gmail.com>
2025-05-04 14:34:18 +0500

    update metadata

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,40 +1,41 @@
-import React from "react";
-import Sidebar from "@/components/Sidebar";
-import Content from "@/components/Content";
-import adsImg from "../../public/images/ads.jpg";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
+import Sidebar from '@/components/Sidebar'
+import Content from '@/components/Content'
+import adsImg from '../../public/images/ads.jpg'
+import Image from 'next/image'
+import Link from 'next/link'
 export default function Page({
   searchParams,
 }: {
-  searchParams?: { query?: string };
+  searchParams?: { query?: string }
 }) {
-  const query = searchParams?.query || "";
+  const query = searchParams?.query || ''
   return (
     <>
-      <div className="flex gap-4">
+      <div className='flex gap-4'>
         <div>
           <Sidebar />
         </div>
-        <div className="max-w-2xl ">
+        <div className='max-w-2xl '>
           <Content query={query} />
         </div>
-        <div className="hidden md:block">
+        <div className='hidden md:block'>
           <Link
-            href="https://www.youtube.com/@EducationWithHamza-g8v"
-            target="_blank"
+            href='https://www.youtube.com/@EducationWithHamza-g8v'
+            target='_blank'
           >
-            <h1 className="text-xl font-bold mb-3">Advertisement</h1>
+            <h1 className='text-xl font-bold mb-3'>Advertisement</h1>
             <Image
               src={adsImg}
-              alt="ads"
+              alt='ads'
               height={600}
               width={250}
-              className="object-cover"
+              className='object-cover'
+              priority
             />
           </Link>
         </div>
       </div>
     </>
-  );
+  )
 }

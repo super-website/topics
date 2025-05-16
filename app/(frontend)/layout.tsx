54a0436226da8e8edb0d2578a6ff4a1a8295ac7a
super-website <kh.muhavia1@gmail.com>
2025-05-16 20:51:01 +0500

    update metadata

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,24 +1,24 @@
-import Footer from "@/components/Footer";
-import HomeNavbar from "@/components/HomerNavbar";
-import React, { Suspense } from "react";
+import Footer from '@/components/Footer'
+import HomeNavbar from '@/components/HomerNavbar'
+import React, { Suspense } from 'react'
 
 export default function HomeLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <div>
-      <Suspense fallback={<p className="loading-spinner"></p>}>
+      <Suspense fallback={<p className='loading-spinner'></p>}>
         <HomeNavbar />
       </Suspense>
       <div
-        className="max-w-7xl mx-auto
-      py-10 min-h-screen"
+        className='max-w-7xl mx-auto 
+      py-10 min-h-screen'
       >
         {children}
       </div>
       <Footer />
     </div>
-  );
+  )
 }

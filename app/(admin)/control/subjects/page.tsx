de2b8309beb5e424eeb71ee0858c03665940683a
super-website <kh.muhavia1@gmail.com>
2025-08-12 22:36:55 +0500

    update metadata

--- app/(admin)/control/subjects/page.tsx
+++ app/(admin)/control/subjects/page.tsx
@@ -1,22 +1,22 @@
-import DeleteBtn from '@/components/DeleteBtn'
-import { addClass, deleteSubject, getAllSubject } from '@/utils/actions'
-import Link from 'next/link'
-import React, { Suspense } from 'react'
+import DeleteBtn from "@/components/DeleteBtn";
+import { deleteSubject, getAllSubject } from "@/utils/actions";
+import Link from "next/link";
+import React, { Suspense } from "react";
 
 export default async function page() {
-  const data = await getAllSubject()
+  const data = await getAllSubject();
 
   return (
     <div>
-      <div className='flex justify-between items-center flex-wrap'>
-        <h1 className='text-sm uppercase '>Subjects</h1>
-        <Link href='subjects/add-subject' className='btn btn-primary'>
+      <div className="flex justify-between items-center flex-wrap">
+        <h1 className="text-sm uppercase ">Subjects</h1>
+        <Link href="subjects/add-subject" className="btn btn-primary">
           Add New
         </Link>
       </div>
-      <hr className='my-10 bg-white p-1 ' />
-      <div className='overflow-x-auto'>
-        <table className='table'>
+      <hr className="my-10 bg-white p-1 " />
+      <div className="overflow-x-auto">
+        <table className="table">
           <thead>
             <tr>
               <th>Subject Name</th>
@@ -31,11 +31,11 @@ export default async function page() {
                 <tr key={subject.id}>
                   <td>{subject.name}</td>
                   <td>{subject.short_name}</td>
-                  <td className='max-w-2xl '>{subject.short_desc}</td>
-                  <td className='space-x-4 flex'>
+                  <td className="max-w-2xl ">{subject.short_desc}</td>
+                  <td className="space-x-4 flex">
                     <Link
                       href={`subjects/edit-subject/${subject.id}`}
-                      className='btn btn-primary'
+                      className="btn btn-primary"
                     >
                       Edit
                     </Link>
@@ -44,11 +44,11 @@ export default async function page() {
                     </Suspense>
                   </td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
